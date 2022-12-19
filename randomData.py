fdbfbde7d19e16199ed3843e743d3285cd5194b7
mkumar-02 <mkumar6@ch.iitr.ac.in>
2022-12-19 11:14:32 +0530

    edited name and email combination

--- randomData.py
+++ randomData.py
@@ -47,8 +47,47 @@ for i in range(0,totalcolumns):
         for j in range(data["fieldmappings"][i]["number"]):
             fieldList[i] += [dateGenerator.dateGenerate(minDate, maxDate)]
 
+    elif(datafileList[i] == 'name'):
+        for j in range(data['fieldmappings'][i]['number']):
+            combinedNameList = ''
+            with open(f'./Data/Philippines/{datafileList[i]}','r') as f:
+                lines = f.readlines()
+
+                for j in range(data['fieldmappings'][i]['combination']):
+                    if(len(combinedNameList) == 0):
+                        combinedNameList += random.choice(lines).strip()
+                    else:
+                        combinedNameList += f"{data['fieldmappings'][i]['separater']}" + random.choice(lines).strip()
+    
+            fieldList[i] += [combinedNameList]
+    
+    elif(datafileList[i] == 'address'):
+        for j in range(data['fieldmappings'][i]['number']):
+            combinedAddressList = ''
+            with open(f'./Data/Philippines/{datafileList[i]}','r') as f:
+                lines = f.readlines()
+
+                for j in range(data['fieldmappings'][i]['combination']):
+                    if(len(combinedAddressList) == 0):
+                        combinedAddressList += random.choice(lines).strip()
+                    else:
+                        combinedAddressList += f"{data['fieldmappings'][i]['separater']}" + random.choice(lines).strip()
+    
+            fieldList[i] += [combinedAddressList]
+
+    elif(datafileList[i] == 'email'):
+        domainList = ['gmail.com', 'yahoo.com']
+        for j in range(data['fieldmappings'][i]['number']):
+            emailList = ''
+            with open('./Data/Philippines/name','r') as f:
+                lines = f.readlines()
+
+                emailList += random.choice(lines).strip().lower() + str(randomInt.randD_digit(4))+ random.choice(domainList)
+        
+            fieldList[i] += [emailList]
+
     else:
-        with open(f'./Data/Philippines/{datafileList[i]}.txt','r') as f:
+        with open(f'./Data/Philippines/{datafileList[i]}','r') as f:
             lines = csv.reader(f)
 
             for j in lines:
@@ -61,6 +100,7 @@ for i in range(0,totalcolumns):
 
 #-------------------------- for csv generation --------------------------------------------------------
 
+# Method:1 -- More space complexity
 with open('csvFile.csv','w',newline="") as f:
     thewriter = csv.writer(f, delimiter=f"{data['delimeter']}")
 
@@ -71,3 +111,23 @@ with open('csvFile.csv','w',newline="") as f:
     for i in range(0, n):
         row_data = [random.choice(j) for j in fieldList]
         thewriter.writerow(row_data)
+
+
+# Method:2 -- More time Complexity
+# with open('csvFile.csv','w',newline="") as f:
+#     thewriter = csv.writer(f, delimiter=f"{data['delimeter']}")
+
+#     thewriter.writerow(headerfields)
+
+#     n = int(input('No. of records: '))
+
+#     for i in range(n):
+#         row_data = []
+#         for k in range(totalcolumns):
+#             with open(f"./Data/Philippines/{data['fieldmappings'][k]['type']}",'r') as df:
+
+#                 lines = df.readlines()
+
+#                 row_data += [random.choice(lines).strip()]
+            
+#         thewriter.writerow(row_data)  
